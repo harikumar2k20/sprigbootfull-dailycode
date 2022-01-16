e8f11ed4ee2477f01ce9480ea1aef7a1f5438b48
harikumar2k20 <harikumar2k20@gmail.com>
2022-01-16 15:27:12 +0530

    Update DepartmentController.java

--- src/main/java/com/dailycode/springbootfull/controller/DepartmentController.java
+++ src/main/java/com/dailycode/springbootfull/controller/DepartmentController.java
@@ -1,8 +1,15 @@
 package com.dailycode.springbootfull.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DepartmentController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Venkat anil";		
+	}
+	
 	
 }
